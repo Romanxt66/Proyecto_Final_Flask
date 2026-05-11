7f1af5ee4b61b82fd7bc3c2d3cf7bf483e8259d4
Robinson Ramiro Aguilar Velasco <aguilarr650@gmail.com>
2026-05-11 15:28:22 -0500

    Update run.py

--- run.py
+++ run.py
@@ -8,3 +8,4 @@ with app.app_context():
 if __name__ == '__main__':
     #socketio.run(app, host="0.0.0.0", port=80, debug=True, allow_unsafe_werkzeug=True)
     app.run(debug=True, host='0.0.0.0', port=5001)
+    
